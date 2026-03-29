673336e9119a6e75df7b878e4c059fd93873ce24
Garen <Garen.Zheng@cn.ey.com>
2026-03-30 00:27:54 +0800

    feat:同事新建文件，且修改自己旧文件代码以后的提交

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import NewCpns from './components/newCpns.vue';
+import NweCpns2 from './components/NweCpns2.vue';
 </script>
 
 <template>
   <HelloWorld />
   <NewCpns></NewCpns>
+  <NweCpns2></NweCpns2>
 </template>
---
+++ src/components/NweCpns2.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="blue">
+        newCpns2
+    </div>
+</template>
+
+<style scoped>
+    .blue{
+        color:blue
+    }
+</style>
