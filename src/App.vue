9dc7e64d19b8b20635d24da1fc7e830d7ad06a33
Garen <Garen.Zheng@cn.ey.com>
2026-03-29 21:00:39 +0800

    feat:模拟同事的新文件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import NewCpns from './components/newCpns.vue';
 </script>
 
 <template>
   <HelloWorld />
+  <NewCpns></NewCpns>
 </template>
---
+++ src/components/NewCpns.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="red">123</div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .red{
+        color:red;
+    }
+</style>
